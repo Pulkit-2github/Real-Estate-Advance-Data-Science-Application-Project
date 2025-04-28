f31b964ff38cc82e0dcf03d50d9fb90ef59ade29
Pulkit Mehrotra <166308034+Pulkit-2github@users.noreply.github.com>
2025-04-28 14:33:19 +0530

    3_Appartments_Recommendation.py

--- 3_Appartments_Recommendation.py
+++ 3_Appartments_Recommendation.py
@@ -5,11 +5,11 @@ import numpy as np
 
 st.set_page_config(page_title="Recommend Appartments")
 
-location_df = pickle.load(open(r"C:\Users\User\Downloads\location_distance.pkl",'rb'))
+location_df = pickle.load(open('location_distance.pkl','rb'))
 
-cosine_sim1 = pickle.load(open(r"C:\Users\User\Downloads\cosine_sim1.pkl",'rb'))
-cosine_sim2 = pickle.load(open(r"C:\Users\User\Downloads\cosine_sim2.pkl",'rb'))
-cosine_sim3 = pickle.load(open(r"C:\Users\User\Downloads\cosine_sim3.pkl",'rb'))
+cosine_sim1 = pickle.load(open('cosine_sim1.pkl','rb'))
+cosine_sim2 = pickle.load(open('cosine_sim2.pkl','rb'))
+cosine_sim3 = pickle.load(open('cosine_sim3.pkl','rb'))
 
 
 def recommend_properties_with_scores(property_name, top_n=5):
