3200e761a9b8a646189fdbcc1f542e1f74a40ad6
Pulkit Mehrotra <166308034+Pulkit-2github@users.noreply.github.com>
2025-04-28 14:36:23 +0530

    1_Price_Predictor.py

--- 1_Price_Predictor.py
+++ 1_Price_Predictor.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Viz Demo")
 with open('df.pkl','rb') as file:
     df = pickle.load(file)
 
-with open('pipeline.pkl','rb') as file:
+with open('pipeline.pkl.gz','rb') as file:
     pipeline = pickle.load(file)
 
 
